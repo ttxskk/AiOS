005a98dc61560a936d228d0391dcce52582849be
ttxskk <sunqingping@sensetime.com>
2024-07-15 23:05:01 +0800

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,7 @@
 python -m torch.distributed.launch \
     --nproc_per_node 1 \
     main.py \
-    --output_dir "/mnt/AFS_sunqingping/log/exp7_" \
-    -c "config/aios_smplx_debug.py" \
+    --output_dir "data/log/" \
+    -c "config/edpose_smplx.cfg.pretrain.py" \
     --options batch_size=2 backbone="resnet50" \
-    --pretrain_model_path data/log/exp0/checkpoint0026.pth
+    --pretrain_model_path data/checkpoint/edpose_r50_coco.pth
